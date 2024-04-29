736ed497e6caab58f402c31d97c9c08b6e45e641
PC\PC <joeltovarx@gmail.com>
2024-04-29 00:17:51 -0600

    Conexion-db

---
+++ MisTareas/Datos/TareasDatos.cs
@@ -0,0 +1,70 @@
+﻿using MisTareas.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MisTareas.Datos
+{
+    public class TareasDatos
+    {
+        public List<TareasModel> listarTareas()
+        {
+            var listaTareas = new List<TareasModel>();
+            var conn = new Conexion();
+
+            using (var conexion = new SqlConnection(conn.GetCadenaSQL())){
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_listar_tareas", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        listaTareas.Add(new TareasModel() {
+                            idTareas = Convert.ToInt32(dr["idTarea"]),
+                            fecha = dr["fecha"].ToString(),
+                            descripcion = dr["descripcion"].ToString(),
+                            estado = Convert.ToInt32(dr["estado"]),
+                            createdAt = dr["createdAt"].ToString(),
+                            updatedAt = dr["updatedAt"].ToString()
+
+                        }) ;
+                    }
+                }
+
+                return listaTareas;
+            }
+        }
+
+        public  TareasModel obtenerTarea(int idTarea)
+        {
+            var resultTarea = new TareasModel();
+            var conn = new Conexion();
+
+            using (var conexion = new SqlConnection(conn.GetCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_obtener_tarea", conexion);
+                cmd.Parameters.AddWithValue("idTarea",idTarea);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        resultTarea.idTareas = Convert.ToInt32(dr["idTarea"]);
+                        resultTarea.fecha = dr["fecha"].ToString();
+                        resultTarea.descripcion = dr["descripcion"].ToString();
+                        resultTarea.estado = Convert.ToInt32(dr["estado"]);
+                        resultTarea.createdAt = dr["createdAt"].ToString();
+                        resultTarea.updatedAt = dr["updatedAt"].ToString();
+                    }
+                }
+
+                return resultTarea;
+            }
+        }
+
+    }
+}
---
+++ MisTareas/Models/TareasModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MisTareas.Models
+{
+    public class TareasModel
+    {
+        public int idTareas { get; set; }
+        public string? fecha { get; set; }
+        public string? descripcion { get; set; }
+        public int estado { get; set; }
+        public string? createdAt { get; set; }
+        public string? updatedAt { get; set; }
+    }
+}
