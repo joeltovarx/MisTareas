79b3eb43ccdf14bdefd6b209009e37b93aaedd82
PC\PC <joeltovarx@gmail.com>
2024-04-28 21:26:48 -0600

    commit-inicial

---
+++ MisTareas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MisTareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
